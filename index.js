b43de9780b430d788f34387d5c0023e6e4b4d112
Will <rrozario958@gmail.com>
2024-02-26 01:52:46 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     exposedHeaders: ["set-cookie"],
   })
 );
