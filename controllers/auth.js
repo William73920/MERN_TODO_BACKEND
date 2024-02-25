105920accd366300abe73819fc8dcb0912c7eb98
Will <rrozario958@gmail.com>
2024-02-26 02:22:09 +0530

    cors

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,6 +46,7 @@ export const signin = async (req, res, next) => {
       .cookie("access_token", token, {
         httpOnly: true,
         sameSite: "none",
+        secure: true,
       })
       .status(200)
       .json(others);
